9a9777898f6e677ea73d05915d73ce0471bb5d0b
emtechprojects <emtech.home@gmail.com>
2025-01-18 17:18:24 +1100

    Content and style update. Added animations.

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ const inner = document.querySelector(".body-inner")
 const input = document.querySelector("input")
 const button = document.querySelector("button")
 
-button.addEventListener('click', (ev) => {
-    doThis(inner, input.value)
-})
+// button.addEventListener('click', (ev) => {
+//     doThis(inner, input.value)
+// })
 
 let randInt = Math.floor(Math.random()*4)
 let items = [
@@ -15,3 +15,24 @@ let items = [
     {imgSrc: "./images/slides/mini-mering.png",title: "12 Pack Mini Meringues"},
     {imgSrc: "./images/slides/vegan-platter.png",title: "24 Pack Vegan Paleo Platter"},
 ]
+
+window.addEventListener('scroll', function () {
+    reveal()
+});
+
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal');
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 0;
+
+        if (revealTop < windowHeight - revealPoint) {
+            reveals[i].classList.add('active');
+        }
+        else {
+            reveals[i].classList.remove('active');
+        }
+    }
+};
